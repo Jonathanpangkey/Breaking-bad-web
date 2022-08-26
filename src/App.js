a6d39fe508a9dff01009e3b77d7dfa770ac7951d
Jonathan pangkey <jonathanpangkey912@gmail.com>
2022-08-27 06:22:51 +0700

    finish project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
+import Search from "./components/ui/Search";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	// set character item
+	const [items, setItems] = useState([]);
+	// set is loading or not
+	const [isLoading, setIsLoading] = useState(true);
+	// set the query for search
+	const [query, setQuery] = useState('');
+
+
+	useEffect(() => {
+		const fetchItems = async () => {
+			const result = await axios(
+				`https://www.breakingbadapi.com/api/characters?name=${query}`
+			);
+
+			// console.log(result.data)
+			setItems(result.data)
+			setIsLoading(false)
+		};
+
+		fetchItems();
+	},
+	//dependency meaning that gonna fire off everytime that's change
+	 [query]);
+
+	return (
+		<div className="container">
+			<Header />
+			<Search getQuery={ (q)=> setQuery(q) } />
+			<CharacterGrid isLoading={isLoading} items={items}/>
+		</div>
+	);
+};
 
 export default App;
